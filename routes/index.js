22235e7659ac04858332752e4c27adc1a69107f0
wangsong <wangsong1299@163.com>
2015-09-06 21:20:48 +0800

    init parts

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+router.get('/abc', function(req, res, next) {
+  res.send('The time is ' + new Date().toString());
+});
+
+router.all('/user/:username', function(req, res,next) { 
+		next();
+});
+
+router.get('/user/:username', function(req, res) { 
+	res.send('user333: ' + req.params.username);
+});
